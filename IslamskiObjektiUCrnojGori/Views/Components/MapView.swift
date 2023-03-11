74b76ce34e166ffd5982a72dfccb48ca646e8283
Marko Sentivanac <markosentivanac@Markos-MacBook-Pro.local>
2023-03-11 17:42:03 +0100

    a lot of stuff added

--- IslamskiObjektiUCrnojGori/Views/Components/MapView.swift
+++ IslamskiObjektiUCrnojGori/Views/Components/MapView.swift
@@ -10,18 +10,38 @@ import MapboxMaps
 import CoreLocation
 
 struct MapBoxMapView: UIViewControllerRepresentable {
-    
-    func makeUIViewController(context: Context) -> MapViewController {
-        return MapViewController()
+    @Binding var allObjects: [ObjectDetails]
+    let didTapOnObject: (ObjectDetails) -> Void
+
+    init(
+        allObjects: Binding<[ObjectDetails]>,
+        didTapOnObject: @escaping (ObjectDetails) -> Void
+    ) {
+        self._allObjects = allObjects
+        self.didTapOnObject = didTapOnObject
     }
     
+    func makeUIViewController(context: Context) -> MapViewController {
+        return MapViewController(didTapOnObject: didTapOnObject)
+    }
+
     func updateUIViewController(_ uiViewController: MapViewController, context: Context) {
+        uiViewController.setPins(for: allObjects)
     }
 }
 
-
-class MapViewController: UIViewController {
+class MapViewController: UIViewController  {
     internal var mapView: MapView!
+    private var pointAnnotationManager: PointAnnotationManager!
+    private var allObjects = [ObjectDetails]()
+    let didTapOnObject: (ObjectDetails) -> Void
+    
+    init(didTapOnObject: @escaping (ObjectDetails) -> Void) {
+        self.didTapOnObject = didTapOnObject
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) { nil }
     
     override public func viewDidLoad() {
         super.viewDidLoad()
@@ -37,15 +57,65 @@ class MapViewController: UIViewController {
         mapView.location.options.puckType = .puck2D()
         mapView.location.locationProvider.startUpdatingLocation()
         
+        pointAnnotationManager = mapView.annotations.makePointAnnotationManager()
+        
+        mapView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onMapClick)))
+
         mapView.mapboxMap.onNext(.mapLoaded) { [weak self] _ in
-            guard let self = self else { return }
-            self.locationUpdate(newLocation: self.mapView.location.latestLocation!)
+            guard let self = self, let latestLocation = self.mapView.location.latestLocation else { return }
+            self.locationUpdate(newLocation: latestLocation)
+        }
+    }
+
+    func setPins(for objects: [ObjectDetails]) {
+        if !objects.isEmpty {
+            self.allObjects = objects
+            let annotations = objects.map {
+                var point = PointAnnotation(
+                    coordinate: CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude)
+                )
+                point.image = .init(image: UIImage(systemSymbol: .pinFill), name: "pin")
+                point.iconAnchor = .bottom
+                return point
+            }
+
+            pointAnnotationManager.annotations = annotations
+            mapView.layoutSubviews()
+        }
+    }
+    
+    
+    @objc private func onMapClick(_ sender: UITapGestureRecognizer) {
+        let point = sender.location(in: mapView)
+        let coordinate = mapView.mapboxMap.coordinate(for: point)
+        let sortedObjects = allObjects.sorted {
+            CLLocation.distance(from: CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude), to: coordinate) < CLLocation.distance(from: CLLocationCoordinate2D(latitude: $1.latitude, longitude: $1.longitude), to: coordinate)
+        }
+        
+        if let first = sortedObjects.first, CLLocation.distance(from: CLLocationCoordinate2D(latitude: first.latitude, longitude: first.longitude), to: coordinate) < 500 {
+            didTapOnObject(first)
         }
     }
 }
 
+
 extension MapViewController: LocationPermissionsDelegate, LocationConsumer {
-    func locationUpdate(newLocation: Location) {
+    func locationUpdate(newLocation: MapboxMaps.Location) {
         mapView.camera.fly(to: CameraOptions(center: newLocation.coordinate, zoom: 10.0), duration: 5.0)
+    }
+}
+
+extension CLLocation {
+    
+    /// Get distance between two points
+    ///
+    /// - Parameters:
+    ///   - from: first point
+    ///   - to: second point
+    /// - Returns: the distance in meters
+    class func distance(from: CLLocationCoordinate2D, to: CLLocationCoordinate2D) -> CLLocationDistance {
+        let from = CLLocation(latitude: from.latitude, longitude: from.longitude)
+        let to = CLLocation(latitude: to.latitude, longitude: to.longitude)
+        return from.distance(from: to)
     }
 }
--- IslamskiObjektiUCrnojGori/Views/HomePageView.swift
+++ IslamskiObjektiUCrnojGori/Views/HomePageView.swift
@@ -7,56 +7,81 @@
 
 import SwiftUI
 import MapboxMaps
+import Combine
 
 struct HomePageView: View {
-    @State var showSideMenu = false
-    
+    typealias Str = TK.HomePageView
+    @State var showLeftSideMenu = false
+    @State var showRightSideMenu = false
+    @ObservedObject var viewModel = ViewModel()
+    @State var selectedObjectDetails: ObjectDetails?
+    @State var isObjectDetailsViewPresented = false
     var body: some View {
-        let drag = DragGesture().onEnded {
-            if $0.translation.width < -100 {
-                withAnimation {
-                    self.showSideMenu = false
-                }
-            }
-        }
-        return NavigationView {
-            GeometryReader { geometry in
-                ZStack(alignment: .leading) {
-                    MainMapView(showSideMenu: self.$showSideMenu)
-                        .frame(width: geometry.size.width, height: geometry.size.height)
-                        .offset(x: self.showSideMenu ? geometry.size.width/2 : 0)
-                        .disabled(self.showSideMenu ? true : false)
-                    
-                    if self.showSideMenu {
-                        ObjectsSideMenuView()
-                            .frame(width: geometry.size.width / 2)
-                            .transition(.move(edge: .leading ))
-                    }
-                }
-                .gesture(drag)
-            }
+        contentView
+            .onAppear(perform: viewModel.fetchAllObjects)
             .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle(Str.objects + " (\(viewModel.allObjects.count))")
             .navigationBarItems(leading: (
                 Button(action: {
                     withAnimation {
-                        self.showSideMenu.toggle()
+                        self.showLeftSideMenu.toggle()
                     }
                 }, label: {
-                    Image(systemName: "line.horizontal.3")
+                    Image(systemSymbol: .line3Horizontal)
                         .imageScale(.large)
                 })
-            ))
-        }
+            ),
+                                trailing: (
+                                    Button(action: {
+                                        
+                                    }, label: {
+                                        Image(systemSymbol: .magnifyingglass)
+                                            .imageScale(.large)
+                                    })
+                                ))
     }
-}
-
-struct MainMapView: View {
-    @Binding var showSideMenu: Bool
     
-    var body: some View {
-        VStack {
-            MapBoxMapView()
-                .ignoresSafeArea()
+    var contentView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                if let selectedObjectDetails {
+                    NavigationLink(
+                        destination: ObjectDetailsView(details: selectedObjectDetails),
+                        isActive: $isObjectDetailsViewPresented) {
+                            
+                        }
+                        .frame(width: 0, height: 0)
+                        .hidden()
+                }
+                MapBoxMapView(
+                    allObjects: $viewModel.allObjects,
+                    didTapOnObject: { details in
+                        selectedObjectDetails = details
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            isObjectDetailsViewPresented = true
+                        }
+                    }
+                )
+                .frame(width: geometry.size.width, height: geometry.size.height)
+                .disabled(self.showLeftSideMenu ? true : false)
+                .blur(
+                    radius: showLeftSideMenu ? 1.0 : 0.0
+                )
+                
+                if self.showLeftSideMenu {
+                    LeftSideMenuView()
+                        .frame(width: geometry.size.width / 1.5)
+                        .transition(.move(edge: .leading ))
+                }
+            }
+            .gesture(
+                DragGesture().onEnded {
+                    if $0.translation.width < -100 {
+                        withAnimation {
+                            self.showLeftSideMenu = false
+                        }
+                    }
+                })
         }
     }
 }
@@ -64,5 +89,21 @@ struct MainMapView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         HomePageView()
+    }
+}
+
+extension HomePageView {
+    final class ViewModel: ObservableObject {
+        private var cancellable: AnyCancellable?
+        @Published var allObjects = [ObjectDetails]()
+        
+        func fetchAllObjects() {
+            cancellable = CompositionRoot.shared.objectsProvider.fetchAllObjects()
+                .sink { completion in
+                    print(completion)
+                } receiveValue: { allObjects in
+                    self.allObjects = allObjects.message
+                }
+        }
     }
 }
